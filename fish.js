9afe0bd5d5fa55aac80e28c48d6c9d1c0c7a512f
Andrew <andrew@andrewt.net>
2021-03-07 14:58:43 +0000

    smol updates

--- fish.js
+++ fish.js
@@ -1,13 +1,13 @@
-var Markov = require('markov-fish'),
+const Markov = require('markov-fish'),
 	https = require('https'),
 	$ = require('cheerio');
 
 module.exports = function Fish(cache, order) {
 	if (!order)
 		order = 2;
-	var allFacts,
-		sanitisedFacts,
-		then = [],
+	let allFacts,
+		sanitisedFacts;
+	const then = [],
 		m = new Markov(order);
 	if (cache) {
 		allFacts = cache;
@@ -16,43 +16,39 @@ module.exports = function Fish(cache, order) {
 		https.get({
 			host: 'github.com',
 			path: '/andrew-t/fish/wiki/List-of-No-Such-Thing-as-a-Fish-Episodes'
-		}, function(resp) {
-			var body = '';
+		}, resp => {
+			let body = '';
 			resp.on('data', function(chunk) { body += chunk; });
 			resp.on('end', function() {
 				allFacts = [];
 				$(body).find('#wiki-body li')
-					.each(function() {
+					.each(() =>
 						allFacts.push($(this).text().trim()
-							.replace(/\s+\([^\(]+\)[^a-z]*$/i, ''));
-					});
-				allFacts = allFacts.filter(function(fact) {
-					return fact != "This is a special \"Worst Of\" episode, consisting of clips removed from the original podcasts.";
-				});
+							.replace(/\s+\([^\(]+\)[^a-z]*$/i, '')));
+				allFacts = allFacts.filter(fact => fact != "This is a special \"Worst Of\" episode, consisting of clips removed from the original podcasts.");
 				generateMarkov();
-			}).on('error', function(err) {
+			}).on('error', err => {
 				console.log('Error:');
 				console.dir(err);
 			});
 		});
+
 	function sanitise(fact) {
 		return fact.toLowerCase().replace(/[^a-z]/g, '').trim();
 	}
+
 	function generateMarkov() {
 		sanitisedFacts = allFacts.map(sanitise);
-		allFacts.forEach(function(fact) {
-			m.train(fact);
-		});
-		then.forEach(function(callback) {
-			callback(m, allFacts);
-		});
+		allFacts.forEach(fact => m.train(fact));
+		then.forEach(callback => callback(m, allFacts));
 	}
-	this.then = function(callback) {
-		allFacts ? callback(m, allFacts)
-			: then.push(callback);
-	};
-	this.getFact = function() {
-		var newFact;
+
+	this.then = callback => allFacts
+		? callback(m, allFacts)
+		: then.push(callback);
+
+	this.getFact = () => {
+		let newFact;
 		do {
 			newFact = m.ramble().trim();
 		} while (~sanitisedFacts.indexOf(sanitise(newFact)) ||
--- hundred.js
+++ hundred.js
@@ -1,12 +1,13 @@
-var Fish = require('./fish.js'),
+const Fish = require('./fish.js'),
 	fs = require('fs'),
 	factCount = 100,
 	order = parseInt(process.argv[2] || '2', 10);
 
-var fish = new Fish(fs.existsSync('facts.json') &&
+const fish = new Fish(fs.existsSync('facts.json') &&
 			JSON.parse(fs.readFileSync('facts.json')),
 		order);
-fish.then(function(m, facts) {
+
+fish.then((m, facts) => {
 	fs.writeFileSync('facts.json', JSON.stringify(facts, null, 2));
 	for (var i = 0; i < factCount; ++i)
 		console.log(i + 1 + '. ' + fish.getFact());
